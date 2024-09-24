a47494a8dd84bc2894d9ff3e5fb927c5a1cc1f40
sergey2k161 <you@example.com>
2024-09-24 18:55:45 +0300

    Laba2 FULL

---
+++ KTPO4310.Shelehov.Service/Program.cs
@@ -0,0 +1,13 @@
+﻿using KTPO4310.Shelehov.Service.LibConfig;
+
+namespace KTPO4310.Ivanov.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogAnalyzerConfig logAnalyzerConfig = new LogAnalyzerConfig();
+            logAnalyzerConfig.TestFiles(); // Вызов метода для проверки файлов
+        }
+    }
+}
